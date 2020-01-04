d154cf68b209eec1fa100bb1c4186f01f96be4d7
Kyle Bjordahl <kyle@lucid.rocks>
2020-01-03 17:11:46 -0800

    chore: clean challenge file

--- src/arrayFunctions/challenge.ts
+++ src/arrayFunctions/challenge.ts
@@ -11,34 +11,40 @@ import { Person } from './types'
 
 /**
  * Return an array of all the people in the provided array who know typescript
+ *
  * @param people an array of Person type objects. See ./types for person description
  */
 export function allCodersWhoKnowTypescript(people: Person[]): Person[] {
 
-	return people.filter(p => p.languages.includes('typescript'))
+	return people
 }
 
+/**
+ * Return an object where the keys are city names, and the values are arrays of the
+ * people from that city
+ *
+ * @param people an array of Person type objects. See ./types for person description
+ */
 export function objectWithPeopleByCity(people: Person[]): object {
 
-	return people.reduce((cities, person) => {
-		return {
-			...cities,
-			[person.city]: [
-				...(Reflect.has(cities, person.city) ? cities[person.city] : []),
-				person,
-			],
-		}
-	}, {})
+	return {}
 }
 
+/**
+ * Return an object where the keys are the language names known by coders in the list,
+ * and the values are the number of developers who know that language
+ *
+ * @param people an array of Person type objects. See ./types for person description
+ */
 export function objectWithCountsOfNumberOfDevelopersWhoKnowEachLanguage(people: Person[]): object {
 
-	return people.reduce((languages, person) => {
-		person.languages.forEach(l => languages[l] ? languages[l] += 1 : languages[l] = 1)
-		return languages
-	}, {})
+	return {}
 }
 
+/**
+ * Return an array of coders, sorted by net worth, ascending.
+ * @param people an array of Person type objects. See ./types for person description
+ */
 export function codersSortedByNetWorth(people: Person[]): Person[] {
-	return people.filter(p => p.coder).sort((a, b) => a.netWorth - b.netWorth)
+	return people
 }
