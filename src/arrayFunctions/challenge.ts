90371b3a6d732bee76a5898a6171f109e88aec7f
Kyle Bjordahl <kyle@lucid.rocks>
2020-01-03 17:08:00 -0800

    chore: add challenge comment

--- src/arrayFunctions/challenge.ts
+++ src/arrayFunctions/challenge.ts
@@ -1,10 +1,18 @@
+/**
+ * complete the following functions so that all tests pass.
+ * - DO NOT modify the incoming arguments
+ * - DO write efficient algorithms, one of the tests is the running time of the test
+ * - DO NOT import any external libraries
+ *
+ * There is no need to use typescript, but feel free to use it if you like
+ */
+
 import { Person } from './types'
 
 /**
  * Return an array of all the people in the provided array who know typescript
  * @param people an array of Person type objects. See ./types for person description
  */
-
 export function allCodersWhoKnowTypescript(people: Person[]): Person[] {
 
 	return people.filter(p => p.languages.includes('typescript'))
